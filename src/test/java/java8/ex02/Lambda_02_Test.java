95a93a805101f7762eb8340f6982d6ff6504680e
Elen <elen.hayot@gmail.com>
2019-03-25 17:45:58 +0100

    Lambda_02_Test done.

--- src/test/java/java8/ex02/Lambda_02_Test.java
+++ src/test/java/java8/ex02/Lambda_02_Test.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
@@ -17,15 +18,19 @@ import static org.junit.Assert.*;
 public class Lambda_02_Test {
 
     // tag::PersonToAccountMapper[]
-    interface PersonToAccountMapper {
-        Account map(Person p);
+    interface PersonToAccountMapper <T,V> {
+        T map(V p);
     }
     // end::PersonToAccountMapper[]
 
     // tag::map[]
     private List<Account> map(List<Person> personList, PersonToAccountMapper mapper) {
         // TODO implémenter la méthode
-        return null;
+    	List<Account> result = new ArrayList<>();
+    	for(Person a:personList){
+    		result.add((Account) mapper.map(a));
+    	}
+        return result;
     }
     // end::map[]
 
@@ -38,7 +43,15 @@ public class Lambda_02_Test {
 
         // TODO transformer la liste de personnes en liste de comptes
         // TODO tous les objets comptes ont un solde à 100 par défaut
-        List<Account> result = map(personList, null);
+        
+        PersonToAccountMapper mapper = p -> {
+        	Account account = new Account();
+        	account.setBalance(100);
+        	account.setOwner((Person) p);
+        	return account;
+        };
+        
+        List<Account> result = map(personList, mapper);
 
         assertThat(result, hasSize(personList.size()));
         assertThat(result, everyItem(hasProperty("balance", is(100))));
@@ -53,7 +66,7 @@ public class Lambda_02_Test {
         List<Person> personList = Data.buildPersonList(100);
 
         // TODO transformer la liste de personnes en liste de prénoms
-        List<String> result = null;
+        List<String> result = personList.stream().map(p -> p.getFirstname()).collect(Collectors.toList());
 
         assertThat(result, hasSize(personList.size()));
         assertThat(result, everyItem(instanceOf(String.class)));
